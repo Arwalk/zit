ee6c69323c20bd9cc1c2f05149b6f86a4d258038
Arwalk <siragentprovocateurarwalk@gmail.com>
2022-05-31 13:42:34 +0200

    With basic slice done.

--- src/zit.zig
+++ src/zit.zig
@@ -6,6 +6,8 @@ const reverse = std.mem.reverse;
 
 fn integer_to_Bits(raw : anytype, size: usize, allocator : Allocator) !Bits {
     var bits = Bits.init(allocator);
+    errdefer bits.deinit();
+
     var i : usize = 0;
     var copy = raw;
     while (i < size-1) : (i += 1) {
@@ -17,10 +19,24 @@ fn integer_to_Bits(raw : anytype, size: usize, allocator : Allocator) !Bits {
     return bits;
 }
 
+fn slice_to_bits(raw: anytype, allocator: Allocator) !Bits {
+    var bits = Bits.init(allocator);
+    errdefer bits.deinit();
+
+    for(raw) |item| {
+        var temp : Bits = try to_Bits(item, allocator);
+        defer temp.deinit();
+        
+        try bits.appendSlice(temp.items);
+    }
+    return bits;
+}
+
 fn to_Bits(raw : anytype, allocator: Allocator) !Bits {
     const typeinfo = @typeInfo(@TypeOf(raw));
     return switch (typeinfo) {
         .Int => try integer_to_Bits(raw, typeinfo.Int.bits, allocator),
+        .Pointer => try slice_to_bits(raw, allocator),
         else => @compileError("Unsupported type")
     };
 }
@@ -70,6 +86,13 @@ pub const Zit = struct {
     }
 
     fn take_int(self: *Zit, comptime T: type) ZitErrors!T {
+        if(T == u1) {
+            if(!self.does_forward_fit(1)) {
+                return ZitErrors.IndexOutOfRange;
+            }
+            defer self.forward(1) catch unreachable;
+            return self.bits.items[self.index];
+        }
         switch(@typeInfo(T).Int.signedness) {
             .signed => @compileError("negative integers unsupported yet"),
             .unsigned => {
@@ -93,7 +116,7 @@ pub const Zit = struct {
     pub fn take(self: *Zit, comptime T: type) ZitErrors!T {
         return switch (@typeInfo(T)) {
             .Int => try self.take_int(T),
-            else => @compileError("Unsupport type to take"),
+            else => @compileError("Unsupported type to take"),
         };
     }
 };
@@ -102,15 +125,34 @@ const testing = std.testing;
 const testing_allocator = testing.allocator;
 const expect = testing.expect;
 const expectEqual = testing.expectEqual;
+const expectError = testing.expectError;
 
 test "with_u32" {
     const value : u32 = 0xFFFF;
     var zitter = try zit(value, testing_allocator);
+    defer zitter.deinit();
+
     var first : u16 = try zitter.take(u16);
     var second : u16 = try zitter.take(u16);
 
+    try expectError(ZitErrors.IndexOutOfRange, zitter.take(u1));
+
     try expectEqual( @intCast(u16, 0), first);
     try expectEqual( @intCast(u16, 0xFFFF), second);
+}
 
+test "with_u8_slice" {
+    const base = [_]u8{0x00, 0xFF, 0x00, 0xFF};
+    var zitter = try zit(base[0..], testing_allocator);
     defer zitter.deinit();
+
+    var first = try zitter.take(u8);
+    var second = try zitter.take(u8);
+    var third = try zitter.take(u8);
+    var fourth = try zitter.take(u8);
+
+    try expectEqual(@intCast(u8, 0x00), first);
+    try expectEqual(@intCast(u8, 0xFF), second);
+    try expectEqual(@intCast(u8, 0x00), third);
+    try expectEqual(@intCast(u8, 0xFF), fourth);
 }
